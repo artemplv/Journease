d9b2f3b1da447a2c5b832305bc6ce8497d6ecd2e
Viktoria Czaran <vczaran@gmail.com>
2023-08-06 21:24:31 -0400

    Add routes and validations, bug in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,90 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const debug = require('debug');
+const cors = require('cors');
+const csurf = require('csurf');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('./models/User');
+require('./config/passport'); 
+const passport = require('passport');
 
-var app = express();
+const { isProduction } = require('./config/keys');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(passport.initialize());
+
+// Security Middleware
+if (!isProduction) {
+  app.use(cors());
+}
+
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
+
+app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
+
+
+if (isProduction) {
+  const path = require('path');
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
