9d89fa14234824a4b30fdd056b8198d6be98e50c
Viktoria Czaran <vczaran@gmail.com>
2023-08-08 15:08:01 -0400

    Users store

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -1,31 +1,32 @@
 import jwtFetch from "./jwt";
 
 const RECEIVE_USER = "users/RECEIVE_USER";
-const RECEIVE_USER_ITINERARIES = "users/RECEIVE_USER_ITINERARIES";
+// const RECEIVE_USER_ITINERARIES = "users/RECEIVE_USER_ITINERARIES";
 
 
-const receiveUser = user => ({
+const receiveUser = (user, userItineraries) => ({
     type: RECEIVE_USER,
-    user
-})
-
-const receiveUserItineraries = userItineraries => ({
-    type: RECEIVE_USER_ITINERARIES,
+    user,
     userItineraries
 })
+
+// const receiveUserItineraries = userItineraries => ({
+//     type: RECEIVE_USER_ITINERARIES,
+//     userItineraries
+// })
 
 
 export const fetchUser = (userId) => async dispatch => {
     const res = await jwtFetch(`/api/users/${userId}`);
     const data = await res.json();
-    dispatch(receiveUser(data.user));
+    dispatch(receiveUser(data.user, data.userItineraries));
 }
 
-export const fetchUserItineraries = (userId) => async dispatch => {
-    const res = await jwtFetch(`/api/users/${userId}/itineraries`);
-    const data = await res.json();
-    dispatch(receiveUserItineraries(data.userItineraries));
-}
+// export const fetchUserItineraries = (userId) => async dispatch => {
+//     const res = await jwtFetch(`/api/users/${userId}/itineraries`);
+//     const data = await res.json();
+//     dispatch(receiveUserItineraries(data.userItineraries));
+// }
 
 
 export default function usersReducer (state = {}, action) {
@@ -33,9 +34,9 @@ export default function usersReducer (state = {}, action) {
 
     switch(action.type) {
         case RECEIVE_USER:
-            return {...newState, [action.user._id]: action.user};
-        case RECEIVE_USER_ITINERARIES:
-            return {...newState, userItineraries: action.userItineraries};
+            return {...newState, [action.user._id]: action.user, userItineraries: action.userItineraries};
+        // case RECEIVE_USER_ITINERARIES:
+        //     return {...newState, userItineraries: action.userItineraries};
         default:
             return state;
     }
