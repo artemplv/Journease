2c2328807372e74882799f8520b577aa005259ae
Viktoria Czaran <vczaran@gmail.com>
2023-08-10 11:35:54 -0400

    Fix folder casing

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,5 +1,5 @@
 import './SplashPage.css';
-import ItineraryIndex from '../itineraryIndex/ItineraryIndex';
+import ItineraryIndex from '../ItineraryIndex/ItineraryIndex';
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import { Modal } from '../../context/Modal';
