82fd835cf3c5c3b0cbcd39ecf6f2d64b9385f5b4
ashley <ashleykimca@gmail.com>
2023-08-10 10:18:30 -0400

    fix nav bar

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,67 @@
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import { useState } from 'react';
+import './NavBar.css';
+import LoginForm from '../SessionForms/LoginForm';
+import SignupForm from '../SessionForms/SignupForm';
+import { Modal } from '../../context/Modal';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+function NavBar () {
+  const loggedIn = useSelector(state => !!state.session.user);
+  const [modalType, setModalType] = useState("")
+  const history = useHistory();
+
+  const signup = () => {
+    setModalType("signup")
+  }
+
+  const login = () => {
+    setModalType("login")
+  }
+
+  const getLinks = () => {
+    if (loggedIn) {
+      return (
+        <ProfileButton/>
+      );
+    } else {
+      return (
+        <div className="links-auth">
+          <button onClick={signup}>Sign Up</button>
+          <button onClick={login}>Log In</button>
+        </div>
+      );
+    }
+  }
+
+  const redirectHome = () => {
+    history.push('/')
+  }
+
+  return (
+    <div id='nav-bar'>
+      <h1 onClick={redirectHome}>
+        <img id="logo" src="https://journease-artemplv.s3.amazonaws.com/public/favicon.png"/></h1>
+      { getLinks() }
+      {(modalType === "signup") && (
+        <Modal onClose={()=> setModalType("")}>
+          <SignupForm
+            openSignInForm={login}
+            closeModal={()=> setModalType("")}
+          />
+        </Modal>
+      )}
+      {(modalType === "login") && (
+        <Modal onClose={()=> setModalType("")}>
+            <LoginForm
+              openSignUpForm={signup}
+              closeModal={()=> setModalType("")}
+            />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default NavBar
