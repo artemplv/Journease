2891a477deedcc19e7949499c00e8dcac6313add
Artem Polevoy <48654322+artemplv@users.noreply.github.com>
2023-08-10 12:01:36 -0400

    fix casing

--- frontend/src/components/ItineraryShow/ItineraryShow.js
+++ frontend/src/components/ItineraryShow/ItineraryShow.js
@@ -6,7 +6,7 @@ import ActivityIndex from "../ItineraryActivities/ActivityIndex";
 import DateNav from "./DateNav";
 import './ItineraryShow.css'
 import MapWrapper from "./Map";
-import UserInfo from "../userProfile/UserInfo";
+import UserInfo from "../UserProfile/UserInfo";
 
 export default function ItineraryShow () {
     const dispatch = useDispatch();
