389787d50e232f38e857ce2a75fd9dbe3303cb61
ashley <ashleykimca@gmail.com>
2023-08-21 14:35:21 -0400

    fix search for empty query, search on enter keydown

--- frontend/src/components/SearchResultPage/SearchResultPage.js
+++ frontend/src/components/SearchResultPage/SearchResultPage.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import ItineraryIndexItem from "../ItineraryIndex/ItineraryIndexItem";
 import './SearchResultPage.css';
 import SearchItinerariesInput from "../SearchItineraries/SearchItinerariesInput";
@@ -8,12 +9,20 @@ const SearchResultPage = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const query = searchParams.get("title");
+    const [queryString, setQueryString] = useState(query);
     const itineraries = Object.values(useSelector(state => state.search));
   
+    useEffect(() => {
+        if (query === "") {
+            setQueryString("recent itineraries")
+        } else {
+            setQueryString(`"${query}"`)
+        }
+    })
     return (
         <div className="search-results-page">
             <div className="search-page-headers">
-                <h1>Search Results for "{query}":</h1>
+                <h1>Search Results for {queryString}:</h1>
                 <SearchItinerariesInput />
             </div>
             <div className="search-results-container">
